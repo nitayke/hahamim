7a634157dbdc3aa324f9ab550eac9a7c84bb76c8
nitayke <nitayke@gmail.com>
2023-06-04 02:56:22 +0300

    המון הפתעות

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 import {
   getDatabase,
   ref,
+  limitToLast,
+  limitToFirst,
+  remove,
   child,
+  update,
   set,
+  increment,
+  query,
+  orderByChild,
   push,
   get,
 } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
@@ -22,6 +29,7 @@ var firstTime;
 var timeDistance;
 var time = 0;
 var interval;
+var last_record;
 
 let question = document.querySelector(".question");
 let answers = document.querySelectorAll(".answer-btn");
@@ -32,8 +40,7 @@ let score = 0;
 let level = 0;
 let indexes_questions;
 let qRef;
-let count;
-const NUM_OF_QUESTIONS = 6;
+const NUM_OF_QUESTIONS = 1;
 const types = ['easy', 'medium', 'hard', 'exotic'];
 const hebrew_types = ['קל', 'בינוני', 'כבד', 'אקזוטי'];
 
@@ -44,117 +51,6 @@ function timer() {
   var milliseconds = Math.floor((timeDistance % 1000) / 10);
   time = seconds.toFixed() + ":" +  String(milliseconds).padStart(2, '0');
   document.getElementById("time").innerHTML = time;
-}
-
-function startGame() {
-  startContainer.classList.add("scale-down");
-  hide(startContainer);
-  show(gameContainer);
-  showLoader();
-  startLevel();
-}
-
-async function startLevel() {
-  const auth = getAuth();
-  await signInAnonymously(auth);
-  qRef = ref(getDatabase(), "questions/" + types[level]);
-  var snapshot = await get(child(qRef, "count"));
-  count = snapshot.val();
-  questionIndex = 0;
-  indexes_questions = Array.from(Array(count).keys());
-  document.getElementById('level').innerHTML = 'רמת קושי: ' + hebrew_types[level];
-  newQuestion();
-}
-
-async function getQuestion() {
-  const rand = indexes_questions[Math.floor(Math.random() * count)];
-  indexes_questions.splice(indexes_questions.indexOf(rand), 1);
-  const snapshot = await get(child(qRef, String(rand)));
-  return snapshot.val();
-}
-
-async function newQuestion() {
-  if (questionIndex < NUM_OF_QUESTIONS) {  
-    await handleOneQuestion();
-    firstTime = new Date().getTime();
-    interval = setInterval(timer, 51);
-    questionIndex++;
-  } else if (level < 3) {
-    level++;
-    startLevel();
-  }
-  else {
-    endGame();
-  }
-}
-
-async function handleOneQuestion() {
-  showLoader();
-  const question1 = await getQuestion();
-  question.innerHTML = question1.name;
-  rightAnswer = question1.type;
-  hideLoader();
-}
-
-function endGame()
-{
-  hide(gameContainer);
-  show(endContainer);
-  document.querySelector(".score").innerHTML = score;
-}
-
-function addError(errorText) {
-  const errorPlace = document.querySelector(".error-place");
-  errorPlace.previousElementSibling.classList.add("make-it-red");
-  errorPlace.innerHTML = errorText;
-}
-
-async function addRecord() {
-  const name = document.getElementById('name').value;
-  if (name === "")
-  {
-    addError("אומרך להכניס שם");
-    return;
-  }
-  showLoader();
-  const quesionsRef = ref(getDatabase(), "records");
-  const newQuestionRef = push(quesionsRef);
-
-  const auth = getAuth();
-  await signInAnonymously(auth);
-  await set(newQuestionRef, {
-    name: name,
-    score: score
-  });
-  window.location.href = "index.html";
-}
-
-function checkAnswer(ansNum) {
-  clearInterval(interval);
-  chosenAnswer = ansNum;
-  answers.forEach((ans) => ans.classList.add("disable-pointer-events"));
-  if (chosenAnswer == rightAnswer) {
-    answers[chosenAnswer].classList.add("make-it-green");
-    document.querySelector(".after-answer-text").innerHTML = "יפה מאוד";
-    score += Math.floor(100 * Math.pow(Math.E, -0.15*(timeDistance/1000)));
-  } else {
-    answers[chosenAnswer].classList.add("make-it-red");
-    answers[rightAnswer].classList.add("make-it-green");
-    document.querySelector(".after-answer-text").innerHTML = "גרוע";
-  }
-  document.querySelector(".smaller-score").innerHTML = "ניקוד: " + score;
-  show(gameFooter);
-}
-
-function nextQuestion() {
-  answers.forEach((ans) => ans.classList.remove("disable-pointer-events"));
-  if (document.querySelector(".make-it-green")) {
-    answers[chosenAnswer].classList.remove("make-it-red");
-    answers[rightAnswer].classList.remove("make-it-green");
-  }
-  document.querySelector(".game-footer").classList.remove("show-game-footer");
-  hide(gameFooter);
-  newQuestion(level);
 }
 
 function showLoader() {
@@ -173,6 +69,156 @@ function hide(element) {
 
 function show(element) {
   element.hidden = false;
+}
+
+async function startGame() {
+  startContainer.classList.add("scale-down");
+  hide(startContainer);
+  show(gameContainer);
+  showLoader();
+  const auth = getAuth();
+  await signInAnonymously(auth);
+  startLevel();
+}
+
+async function startLevel() {
+  qRef = ref(getDatabase(), "questions/" + types[level]);
+  var snapshot = await get(child(qRef, "count"));
+  const count = snapshot.val();
+  questionIndex = 0;
+  indexes_questions = Array.from(Array(count).keys());
+  document.getElementById('level').innerHTML = 'רמת קושי: ' + hebrew_types[level];
+  newQuestion();
+}
+
+async function newQuestion() {
+  if (questionIndex < NUM_OF_QUESTIONS) {  
+    await handleOneQuestion();
+    firstTime = new Date().getTime();
+    interval = setInterval(timer, 51);
+    questionIndex++;
+  } else if (level < 3) {
+    level++;
+    startLevel();
+  }
+  else {
+    endGame();
+  }
+}
+
+async function getQuestion() {
+  const rand = indexes_questions[Math.floor(Math.random() * indexes_questions.length)];
+  indexes_questions.splice(indexes_questions.indexOf(rand), 1);
+  const snapshot = await get(child(qRef, String(rand)));
+  return snapshot.val();
+}
+
+async function handleOneQuestion() {
+  const question1 = await getQuestion();
+  question.innerHTML = question1.name;
+  rightAnswer = question1.type;
+  hideLoader();
+}
+
+function addError(errorText) {
+  const errorPlace = document.querySelector(".error-place");
+  errorPlace.previousElementSibling.classList.add("make-it-red");
+  errorPlace.innerHTML = errorText;
+}
+
+function checkAnswer(ansNum) {
+  clearInterval(interval);
+  chosenAnswer = ansNum;
+  answers.forEach((ans) => ans.classList.add("disable-pointer-events"));
+  if (chosenAnswer == rightAnswer) {
+    answers[chosenAnswer].classList.add("make-it-green");
+    document.querySelector(".after-answer-text").innerHTML = "יפה מאוד";
+    score += Math.floor(100 * Math.pow(Math.E, -0.15*(timeDistance/1000)));
+  } else {
+    answers[chosenAnswer].classList.add("make-it-red");
+    answers[rightAnswer].classList.add("make-it-green");
+    document.querySelector(".after-answer-text").innerHTML = "גרוע";
+  }
+  document.getElementById("smaller-score").innerHTML = "ניקוד: " + score;
+  show(gameFooter);
+}
+
+function nextQuestion() {
+  showLoader();
+  answers.forEach((ans) => ans.classList.remove("disable-pointer-events"));
+  if (document.querySelector(".make-it-green")) {
+    answers[chosenAnswer].classList.remove("make-it-red");
+    answers[rightAnswer].classList.remove("make-it-green");
+  }
+  document.querySelector(".game-footer").classList.remove("show-game-footer");
+  hide(gameFooter);
+  newQuestion(level);
+}
+
+async function addRecord() {
+  const name = document.getElementById('name').value;
+  if (name === "")
+  {
+    addError("אומרך להכניס שם");
+    return;
+  }
+  showLoader();
+
+  console.log("records/" + Object.keys(last_record)[0]);
+  await remove(ref(getDatabase(), "records/" + Object.keys(last_record)[0]));
+
+  const recordsRef = ref(getDatabase(), "records");
+  const newRef = push(recordsRef);
+
+  await set(newRef, {
+    name: name,
+    score: score
+  });
+  window.location.href = "index.html";
+}
+
+async function get10th()
+{
+  const qRef = ref(getDatabase(), "records");
+  const lowest = query(qRef, orderByChild('score'), limitToFirst(1));
+  const snapshot = await get(lowest);
+  last_record = snapshot.val();
+}
+
+async function getLocation()
+{
+  const score_level = Math.floor(score / 10);
+  var snapshot = await get(child(qRef, 'sum'));
+  var all_sum = snapshot.val();
+  const higher_scores = query(qRef, limitToLast(201 - score_level));
+  snapshot = await get(higher_scores);
+  var sum = 1;
+  snapshot.forEach(child => {
+    sum += child.val();
+  });
+  sum -= all_sum; // because part of the query is the sum of all
+  all_sum++;
+  const updates = {};
+  updates[score_level] = increment(1);
+  updates['sum'] = increment(1);
+  update(qRef, updates);
+  return sum + '/' + all_sum;
+}
+
+async function endGame()
+{
+  showLoader();
+  hide(gameContainer);
+  show(endContainer);
+  qRef = ref(getDatabase(), "scores");
+
+  await get10th();
+  if (score > Object.values(last_record)[0]['score'])
+    show(document.getElementById('enter-record'));
+
+  document.getElementById('location').innerHTML = await getLocation();
+  document.getElementById("score").innerHTML = score;
+  hideLoader();
 }
 
 window.startGame = startGame;
--- python/txt_to_json.py
+++ python/txt_to_json.py
@@ -1,26 +1,27 @@
-from uuid import uuid4
 from json import dumps
-from random import randint
 
 f = open('list.txt', encoding="utf8")
 txt = f.read()
 f.close()
 data = [i.split('\n') for i in txt.split('\n\n')]
 print(data)
-json = {}
+questions = {}
 levels = ['easy', 'medium', 'hard', 'exotic']
 for count, i in enumerate(levels):
     count_for_level = 0
-    json[i] = {}
+    questions[i] = {}
     for count1, j in enumerate(range(count, count + 13, 4)):
         for k in data[j]:
             if k == '':
                 continue
-            json[i][count_for_level] = {"name": k, "type": count1}
+            questions[i][count_for_level] = {"name": k, "type": count1}
             count_for_level += 1
-    json[i]["count"] = count_for_level
+    questions[i]["count"] = count_for_level
+scores = {}
+for i in range(200):
+    scores[i] = 0
 
-j = dumps({"questions": json}, ensure_ascii=False)
+j = dumps({"questions": questions, "scores": scores}, ensure_ascii=False)
 print(j)
 with open('result.json', 'w', encoding="utf-8") as f:
     f.write(j)
