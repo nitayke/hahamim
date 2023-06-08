f6fee41f905d1c2583885ddc53880fe6f2ea2d39
nitayke <nitayke@gmail.com>
2023-06-08 20:07:06 +0300

    firebase config

--- js/utils/firebase-init.js
+++ js/utils/firebase-init.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxafWABzdwnokrYgyZRqCcfVgLDeXM18I",
-  authDomain: "shvushon-hahamim.firebaseapp.com",
-  projectId: "shvushon-hahamim",
-  storageBucket: "shvushon-hahamim.appspot.com",
-  messagingSenderId: "358728914528",
-  appId: "1:358728914528:web:088c3dd33ec38fdd0a9505",
-  measurementId: "G-SBW22SF8ZE",
-  databaseURL: "https://shvushon-hahamim-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "AIzaSyB6WVb4j7vDiioyYi-JgVaq53wfdpeRVJ0",
+  authDomain: "hahamim-d7a18.firebaseapp.com",
+  projectId: "hahamim-d7a18",
+  storageBucket: "hahamim-d7a18.appspot.com",
+  messagingSenderId: "205614945391",
+  appId: "1:205614945391:web:2c8f2f1d75ff625db6e99a",
+  measurementId: "G-LZHC0H8EEQ",
+  databaseURL: "https://hahamim-d7a18-default-rtdb.europe-west1.firebasedatabase.app",
 };
 getAnalytics(initializeApp(firebaseConfig));
