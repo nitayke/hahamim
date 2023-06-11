79fe063b7e2f34ef2005593fa33746813eb479ab
Noam Loewenstern <noamloewenstern@gmail.com>
2023-06-11 19:59:13 +0300

    added 'AddQuestion' page, including validation form

--- src/routes/AddQuestion/AddQuestion.tsx
+++ src/routes/AddQuestion/AddQuestion.tsx
@@ -1,73 +1,116 @@
-import useIsMobile from "~/hooks/useIsMobile";
+import { useForm } from "react-hook-form";
 
+import useIsMobile from "~/hooks/useIsMobile";
+import { IRabbiType, RabbiTypeKeys } from "~/types/types";
+import { useEffect } from "react";
+import { addQuestion } from "~/firebase/lib/add-question";
+import { useGlobalLoadingSpinner } from "~/hooks/useLoadingSpinner";
+import { useNavigate } from "react-router-dom";
+const FormFields = {
+  question: "",
+  answer: "" as IRabbiType,
+  level: "" as "easy" | "medium" | "hard",
+};
 export default function AddQuestion() {
   const isMobile = useIsMobile();
-  // todo use useHook with validation
-  const handleHideMobileMenu = () => {
-    throw new Error("Not implemented");
+  const navigate = useNavigate();
+  const { open: openLoadingSpinner, close: closeLoadingSpinner } = useGlobalLoadingSpinner();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, dirtyFields },
+    setFocus,
+    trigger,
+  } = useForm<typeof FormFields>({
+    mode: "onBlur",
+  });
+
+  const handleAddQuestionSubmit = async (values: typeof FormFields) => {
+    try {
+      openLoadingSpinner();
+      await addQuestion(values);
+      alert("השאלה נשמרה בהצלחה!");
+      navigate("/");
+    } catch (e) {
+      alert("שגיאה בשמירה!");
+    } finally {
+      closeLoadingSpinner();
+    }
   };
-  const handleRemoveValidationFromFormField = (fieldNumber: number) => {
-    throw new Error("Not implemented");
-  };
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    throw new Error("Not implemented");
-  };
+
   return (
-    <div className="all-site" onClick={handleHideMobileMenu}>
+    <>
       <form
         className="flex flex-col items-center justify-start p-4"
-        id="add-question-form"
         autoComplete="off"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit((data) => handleAddQuestionSubmit(data))}
       >
         <h2>הוסף שאלה</h2>
         <div className={`flex items-center justify-around w-4/5 ${isMobile ? "flex-col" : ""}`}>
           <div className={`w-2/5 flex flex-col items-start ${isMobile ? "w-full" : ""}`}>
             <label>השאלה</label>
             <input
-              id="q"
               className="text-box"
-              onFocus={() => handleRemoveValidationFromFormField(0)}
-              onChange={() => handleRemoveValidationFromFormField(0)}
+              placeholder="שם הדמות"
+              {...register("question", {
+                required: true,
+                validate: (value) => /^[^A-Za-z]+$/.test(value),
+                onChange: () => trigger("question"),
+              })}
             />
-            <div className="error-place"></div>
-            <label>מה התשובה?</label>
-            <select
-              defaultValue={-1}
-              className="text-box"
-              id="answer"
-              onChange={() => handleRemoveValidationFromFormField(1)}
-            >
-              <option value="" disabled hidden>
-                בחר תשובה
-              </option>
-              <option value="0">תנא</option>
-              <option value="1">אמורא</option>
-              <option value="2">ראשון</option>
-              <option value="3">אחרון</option>
-            </select>
-            <div className="error-place"></div>
-            <label>דרגת קושי</label>
-            <select
-              defaultValue={-1}
-              className="text-box"
-              id="level"
-              onChange={() => handleRemoveValidationFromFormField(2)}
-            >
-              <option value="" disabled hidden>
-                בחר דרגת קושי
-              </option>
-              <option value="0">קל</option>
-              <option value="1">בינוני</option>
-              <option value="2">כבד</option>
-            </select>
-            <div className="error-place"></div>
+            {errors.question && <div className="error-place">מותר רק שמות בעברית</div>}
+            {!errors?.question && dirtyFields["question"] && (
+              <>
+                <label>מה התשובה?</label>
+                <select
+                  defaultValue={""}
+                  className="text-box"
+                  {...register("answer", {
+                    required: true,
+                    validate: (value) => RabbiTypeKeys.includes(value),
+                    disabled: !!errors?.question,
+                  })}
+                >
+                  <option value="" disabled hidden>
+                    בחר תשובה
+                  </option>
+                  <option value="תנא">תנא</option>
+                  <option value="אמורא">אמורא</option>
+                  <option value="ראשון">ראשון</option>
+                  <option value="אחרון">אחרון</option>
+                </select>
+                {errors.answer && <div className="error-place">תשובה לא אפשרית</div>}
+              </>
+            )}
+            {!errors.question && !errors.answer && dirtyFields["answer"] && (
+              <>
+                <label>דרגת קושי</label>
+                <select
+                  defaultValue={""}
+                  className="text-box"
+                  {...register("level", {
+                    required: true,
+                    validate: (value) => ["easy", "medium", "hard"].includes(value),
+                    disabled: !!errors?.question || !!errors?.answer,
+                  })}
+                >
+                  <option value="" disabled hidden>
+                    בחר דרגת קושי
+                  </option>
+                  <option value="easy">קל</option>
+                  <option value="medium">בינוני</option>
+                  <option value="hard">כבד</option>
+                </select>
+                {errors.level && <div className="error-place">תשובה לא אפשרית</div>}
+              </>
+            )}
           </div>
         </div>
-
-        <input type="submit" className="btn" value="הוסף שאלה" />
+        {isValid && dirtyFields["level"] && (
+          <input type="submit" className="btn mt-4" value="הוסף שאלה" />
+        )}
       </form>
-    </div>
+    </>
   );
 }
