82cbfa2b9fd55cdd04efd9d808261ce9388c58a9
Noam Loewenstern <noamloewenstern@gmail.com>
2023-06-13 04:26:27 +0300

    added record location calculation

--- src/firebase/lib/records.ts
+++ src/firebase/lib/records.ts
@@ -1,5 +1,5 @@
-import { ref, getDatabase, query, orderByChild, get } from "firebase/database";
-import { DBKeys, RecordScore } from "../types";
+import { ref, getDatabase, query, orderByChild, get, increment, update } from "firebase/database";
+import { DB, DBKeys, RecordScore } from "../types";
 
 export async function getRecords() {
   const qRef = ref(getDatabase(), DBKeys.records);
@@ -8,7 +8,9 @@ export async function getRecords() {
   const snapshot = await get(qRecords);
   const records = [] as RecordScore[];
   snapshot.forEach((child) => {
-    records.push(child.val());
+    const record = child.val();
+    if (!record?.name || !record?.score) return;
+    records.push(record);
   });
   return records;
 }
@@ -17,4 +19,27 @@ export async function isRecordInTopRank({ score }: { score: number }) {
   const records = await getRecords();
   records.sort((a, b) => b.score - a.score);
   return records.length < 10 || score > records[records.length - 1].score;
+}
+
+export async function getRecordPosition(originalScore: number) {
+  const score = Math.floor(originalScore / 10);
+  const scoresRef = ref(getDatabase(), DBKeys.scores);
+  const scores = (await get(scoresRef)).val() as DB["scores"];
+  const totalCountPlayed = scores.countPlayed;
+  const scoresInDB = scores.scores as unknown as number[];
+
+  const higherScores = Object.keys(scoresInDB).filter((key) => Number(key) >= score);
+  const countPlayedWhoHaveHigherScore = higherScores.reduce(
+    (acc, key) => acc + scoresInDB[+key],
+    0
+  );
+  console.log("countPlayedWhoHaveHigherScore", countPlayedWhoHaveHigherScore);
+
+  const position = totalCountPlayed - countPlayedWhoHaveHigherScore;
+
+  await update(scoresRef, {
+    [score]: increment(1),
+    countPlayed: increment(1),
+  });
+  return `${position}/${totalCountPlayed + 1}`;
 }
